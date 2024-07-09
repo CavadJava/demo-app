29cffac8d80e4f6247f0398699a1eea53b051ff0
CavadJava <cavadjava@gmail.com>
2024-07-09 11:13:46 +0400

    Init Demo project

---
+++ src/main/java/com/example/demoapp/DemoAppApplication.java
@@ -0,0 +1,18 @@
+package com.example.demoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    public String hello() {return "hello";}
+}
